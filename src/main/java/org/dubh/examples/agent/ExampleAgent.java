edb8da5b0195c46aaf052af7907aedd82342a31f
Brian Duff <cairndubh@gmail.com>
2020-10-04 15:55:44 -0700

    Add comments

--- src/main/java/org/dubh/examples/agent/ExampleAgent.java
+++ src/main/java/org/dubh/examples/agent/ExampleAgent.java
@@ -36,19 +36,38 @@ public class ExampleAgent {
   }
 
   private static byte[] transformClass(byte[] classfileBuffer) {
+    // ClassReader knows how to grok the buffer of bytes as a Java class.
     ClassReader reader = new ClassReader(classfileBuffer);
+
+    // ClassNode is a visitor over the things in the classfile that collects
+    // them into an in-memory data structure that we can easily traverse. You
+    // can also avoid creating a separate in-memory representation by just
+    // implementing a simple ClassVisitor, but it often requires more code.
     ClassNode classNode = new ClassNode();
     reader.accept(classNode, Opcodes.ASM8);
+
+    // Now ClassNode contains a data strcuture with all the things in the
+    // class, and we can look through the methods for the one we care about.
     for (MethodNode method : classNode.methods) {
+      // You'd maybe want to check the signature also in a real program.
       if ("getName".equals(method.name)) {
+        // Method bodies contain instruction lists. Here, we create a simple
+        // instruction list with two instructions - one to call a static 
+        // method, and another to return whatever that static method returned.
         InsnList instructions = new InsnList();
-        instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "org/dubh/examples/agent/NewGreeter", "getName",
+        instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, 
+            "org/dubh/examples/agent/NewGreeter", "getName",
             "()Ljava/lang/String;"));
         instructions.add(new InsnNode(Opcodes.ARETURN));
+
+        // This replaces the existing instruction list of the method with our
+        // new instruction list.
         method.instructions = instructions;
       }
     }
 
+    // ClassWriter is a visitor that knows how to traverse the data structure,
+    // and write back out the bytes of a class.
     ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
     classNode.accept(writer);
 
